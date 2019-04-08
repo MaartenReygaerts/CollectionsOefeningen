a91fc7a8876ff087e6a67c1846c1eb313462ea71
MAARTEN REYGAERTS <MAARTENR@cursisten.intecbrussel.intra>
2019-04-08 13:55:05 +0200

    Collections Maarten

---
+++ src/App2.java
@@ -0,0 +1,48 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class App2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+
+
+        System.out.println("Geef een woord in: ");
+        String zin = input.next();
+        list.add(zin);
+        System.out.println(list);
+
+        boolean end = false;
+        while (!end) {
+            System.out.println("Geef een woord in: ");
+            String zin2 = input.next();
+            list.add(zin2);
+            System.out.println(list);
+            if (zin2.endsWith(".")) {
+                end = true;
+            }
+
+        }
+        System.out.println("***** Druk de zin in omgekeerde volgore af. ******");
+        for (int i =0,j=list.size()-1;i<j;i++){
+            list.add(i,list.remove(j));
+        }
+        //Collections.reverse(list);
+        System.out.println("***** Na de reverse for-loop *****");
+        System.out.println(list);
+
+        System.out.println("***** Count van elementen in de list *****");
+        int count = list.size();
+        System.out.println(count);
+        System.out.println("***** verzameling opsommen in strings *****");
+        Collections.reverse(list);
+       Stream.of(list).forEach(System.out::println);
+
+        list.stream().forEach(System.out::println);
+    }
+}
---
+++ src/MainApp.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        //Maak een programma dat de gebruiker om een aantal getallen vraagt. Gebruik hiervoor de klasse Scanner met haar methode nextInt().
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+
+
+        System.out.println("Geef een getal in :");
+
+        list.add(input.nextInt());
+        list.add(input.nextInt());
+        list.add(input.nextInt());
+        list.add(input.nextInt());
+        list.add(input.nextInt());
+
+
+        System.out.println("De lijst:");
+        System.out.println(list);
+
+        //sum van de getallen in de list
+        int sum = list.stream()
+                .mapToInt(a -> a)
+                .sum();
+        System.out.println("Sum is " + sum);
+
+        //gemiddelde zoeken
+        OptionalDouble average = list.stream()
+                .mapToDouble(value -> value)
+                .average();
+        System.out.println("Average is " + average);
+    }
+}
